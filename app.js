8f13171eab86d2e83b4e6a329750210e31073b0b
Luis Paulo <luisppm@hotmail.com>
2021-11-16 00:22:21 -0300

    adjust app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var app = require('./config/server');
 
-app.get('/', function(req, res){
-    
+app.get('/', function(req, res){    
     console.log('Metodo get');
     res.status(200).send('Método get');
-
 })
 
 app.listen(process.env.PORT || 3000, function(req, res){
