bd03eeb3579d5c71a66b7288ae76e1ad966850bf
Luis Paulo <luisppm@hotmail.com>
2021-11-15 21:39:59 -0300

    Teste server

---
+++ app.js
@@ -0,0 +1,12 @@
+var app = require('./config/server');
+
+app.get('/', function(req, res){
+    
+    console.log('Metodo get');
+    res.status(200).send('Método get');
+
+})
+
+app.listen(3000, function(req, res){
+    console.log("Server rodando porta: 3000");
+})
