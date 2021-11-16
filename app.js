182f1c7f9e759ce158d1d784d9feb666a495a44b
Luis Paulo <luisppm@hotmail.com>
2021-11-15 23:20:47 -0300

    Config port Heroku

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', function(req, res){
 
 })
 
-app.listen(3000, function(req, res){
+app.listen(process.env.PORT || 3000, function(req, res){
     console.log("Server rodando porta: 3000");
 })
